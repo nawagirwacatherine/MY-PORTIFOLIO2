22682b687f23b410618b589c6eb76bbe578371b8
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-02-08 21:34:00 +0200

    add  project images

--- Cathies portifolio/src/pages/HomePage.jsx
+++ Cathies portifolio/src/pages/HomePage.jsx
@@ -1,5 +1,10 @@
 
 import imagesImage2 from "../images/image2.jpg";
+import imagesProject1 from "../images/project1.jpg";
+import imagesProject2 from "../images/project2.jpg";
+import imagesProject3 from "../images/project3.jpg";
+import imagesProject4 from "../images/project4.jpg";
+
 
 function HomePage (){
     return(
@@ -21,13 +26,21 @@ function HomePage (){
 
         <div className="intro2">
           <div>
-          <div className="projects">Project1</div>
-          <div className="projects">Project2</div>
+          <div className="projects">Project1
+          <img src={imagesProject1} alt="images" className="image" />
+          </div>
+          <div className="projects">Project2
+          <img src={imagesProject2} alt="images" className="image" />
+          </div>
           </div>
           
           <div>
-          <div className="projects">Project3</div>
-          <div className="projects">Project4</div>
+          <div className="projects">Project3
+          <img src={imagesProject3} alt="images" className="image" />
+          </div>
+          <div className="projects">Project4
+          <img src={imagesProject4} alt="images" className="image" />
+          </div>
           </div>
 
       </div>
